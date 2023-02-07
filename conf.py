1185539b97c35b412534960ba02c76e42869713f
okmksato <okmikichan@gmail.com>
2023-02-07 20:27:00 +0900

    update

--- conf.py
+++ conf.py
@@ -31,8 +31,8 @@ release = '0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 
-# extensions = ['myst_parser', 'sphinx.ext.githubpages']
-extensions = [sphinx.ext.githubpages']
+extensions = ['myst_parser', 'sphinx.ext.githubpages']
+# extensions = [sphinx.ext.githubpages']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
